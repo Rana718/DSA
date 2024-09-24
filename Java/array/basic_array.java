a221d73bb3efaad40c9d9fb5b6e4cb75732130e5
Rana718 <ranadolui718@gmail.com>
2024-09-24 17:11:24 +0530

    Add the java

---
+++ Java/array/basic_array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class basic_array {
+    public static void main(String[] args) {
+        // Create a Scanner object for reading input
+        Scanner sc = new Scanner(System.in);
+
+        // get user to input the size of the array
+        System.out.println("Enter the size of array");
+        int size = sc.nextInt();
+
+        int[] arr = new int[size]; // create the array
+
+        // input the array elements
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // print the array elements
+        System.out.println("The array elements are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        sc.close(); // close the scanner
+    }
+}
